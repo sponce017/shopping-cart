5dbc7c5940a8a18161970634b7db44565b2a85b6
sponce <poncesaul014@outlook.com>
2025-05-01 23:52:39 -0600

    Fix product controller

--- src/main/java/com/prueba/sponce/controller/ProductController.java
+++ src/main/java/com/prueba/sponce/controller/ProductController.java
@@ -1,10 +1,8 @@
 package com.prueba.sponce.controller;
 
-import com.prueba.sponce.dto.ApiResponse;
 import com.prueba.sponce.dto.ProductDto;
 import com.prueba.sponce.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -17,14 +15,12 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping
-    public Mono<ResponseEntity<ApiResponse<Flux<ProductDto>>>> getAll() {
-        Flux<ProductDto> products = productService.getAllProducts();
-        return Mono.just(ResponseEntity.ok(new ApiResponse<>("Products retrieved successfully", products)));
+    public Flux<ProductDto> getAll() {
+        return productService.getAllProducts();
     }
 
     @GetMapping("/{id}")
-    public Mono<ResponseEntity<ApiResponse<Mono<ProductDto>>>> getById(@PathVariable Long id) {
-        Mono<ProductDto> product = productService.getProductById(id);
-        return Mono.just(ResponseEntity.ok(new ApiResponse<>("Product retrieved successfully", product)));
+    public Mono<ProductDto> getById(@PathVariable Long id) {
+        return productService.getProductById(id);
     }
 }
